4d2df5671791d3fcb396381ea901917c62fa6a14
magdy <mohamedmagdy171199@gmail.com>
2022-11-09 23:30:59 +0200

    Initial commit

---
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroesController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Data;
+using SuperHeroAPI.Models;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public SuperHeroesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateSuperHero(SuperHero hero)
+        {
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateSuperHero(SuperHero hero)
+        {
+            var dbhero = await _context.SuperHeroes.FindAsync(hero.Id);
+            if (dbhero == null)
+                return BadRequest("Not Found");
+
+            dbhero.FirstName=hero.FirstName;
+            dbhero.LastName=hero.LastName;
+            dbhero.Location=hero.Location;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteSuperHero(int id)
+        {
+            var herodb = await _context.SuperHeroes.FindAsync(id);
+            if (herodb == null)
+                return BadRequest("Hero Not Found");
+
+            _context.SuperHeroes.Remove(herodb);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+    }
+}
---
+++ SuperHeroAPI/SuperHeroAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Models;
+
+namespace SuperHeroAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
